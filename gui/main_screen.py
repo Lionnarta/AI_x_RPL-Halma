1a2f7997c41c7859867c4a7b39ca2a04f99ffa6b
Lionnarta <lionnarta.savirandy2000@gmail.com>
2020-10-03 22:37:17 +0700

    update start screen

--- gui/main_screen.py
+++ gui/main_screen.py
@@ -7,7 +7,7 @@ from pygame.locals import *
 pygame.init()
 
 # Set up display resolution
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((1024, 768))
 
 # Title
 pygame.display.set_caption("Halma")
@@ -27,35 +27,61 @@ text_one = text_font.render('8x8', True, text_color)
 text_two = text_font.render('10x10', True, text_color)
 text_three = text_font.render('16x16', True, text_color)
 
+# Variable
+click = False
+x = 0
 # Game loop
 event_running = True
 while event_running:
     # Screen background
     screen.fill((53,50,50))
+
+    # Button
+    button_1 = pygame.Rect(width/2-50,height/2,100,25)
+    button_2 = pygame.Rect(width/2-50,height/2+30,100,25)
+    button_3 = pygame.Rect(width/2-50,height/2+60,100,25)
     
+    click = False
     for event in pygame.event.get():
         if event.type == QUIT:
             event_running = False
+        
+        # check if button clicked
+        if event.type == MOUSEBUTTONDOWN:
+            # button 1
+            if event.button == 1:
+                click = True
     
     # mouse coordinates
     mouse = pygame.mouse.get_pos()
-
+    
+    # Button event
+    if button_1.collidepoint(mouse[0], mouse[1]):
+        if click:
+            x = 1
+    if button_2.collidepoint(mouse[0], mouse[1]):
+        if click:
+            x = 2
+    if button_3.collidepoint(mouse[0], mouse[1]):
+        if click:
+            x = 3
+    
     # Button event on hover
     # Button 1
     if width/2-50 <= mouse[0] <= width/2+50 and height/2 <= mouse[1] <= height/2+25:
-        pygame.draw.rect(screen, button_dark, [width/2-50,height/2,100,25])
+        pygame.draw.rect(screen, button_dark, button_1)
     else:
-        pygame.draw.rect(screen, button_light, [width/2-50,height/2,100,25])
+        pygame.draw.rect(screen, button_light, button_1)
     # Button 2
     if width/2-50 <= mouse[0] <= width/2+50 and height/2+30 <= mouse[1] <= height/2+55:
-        pygame.draw.rect(screen, button_dark, [width/2-50,height/2+30,100,25])
+        pygame.draw.rect(screen, button_dark, button_2)
     else:
-        pygame.draw.rect(screen, button_light, [width/2-50,height/2+30,100,25])
+        pygame.draw.rect(screen, button_light, button_2)
     # Button 3
     if width/2-50 <= mouse[0] <= width/2+50 and height/2+60 <= mouse[1] <= height/2+85:
-        pygame.draw.rect(screen, button_dark, [width/2-50,height/2+60,100,25])
+        pygame.draw.rect(screen, button_dark, button_3)
     else:
-        pygame.draw.rect(screen, button_light, [width/2-50,height/2+60,100,25])
+        pygame.draw.rect(screen, button_light, button_3)
 
     # Generate button text
     screen.blit(text_one, (width/2-12,height/2))

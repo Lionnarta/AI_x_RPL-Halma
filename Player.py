c0c80d817ca09b8f5bdcc9abfbafa60c39c5f75b
Kevin <kevinastefano@gmail.com>
2020-10-03 23:17:15 +0700

    Player, pion, posisi ~Fan

---
+++ Player.py
@@ -0,0 +1,25 @@
+
+import numpy as np
+import Pion
+
+
+class Player:
+    """
+    class untuk agen
+    Terdiri dari pion-pion dalam board
+    """
+
+    def __init__(self):
+        self.noPlayer = noPlayer
+        self.arrayPion = {}
+
+        def addPion(Pion):
+            # masukan harus tipe bentukan Pion
+            self.arrayPion.append(Pion)
+
+        def movePion(pionId, posisi):
+            # masukkan harus berupa tipe bentukkan posisi
+            for pion in self.arrayPion:
+                if pion.pionId == pionId:
+                    pion.currentPosition = posisi
+                    break

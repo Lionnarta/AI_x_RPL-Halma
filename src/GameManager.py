315507d9d496b2c3aebda5de3b60a6d6339589ce
michaelhans <michaelhans777@gmail.com>
2020-10-16 18:29:30 +0700

    Add Utility Function

--- src/GameManager.py
+++ src/GameManager.py
@@ -3,12 +3,13 @@ import Pion
 import Posisi
 import Board
 import Cell
+import Minimax
 
 boardSize = 10
 
 
 class GameManager:
-    def __init__(self):
+    def __init__(self, boardSize):
         self.currentPlayer = Player.Player(1, boardSize)
         self.oppositePlayer = Player.Player(2, boardSize)
         self.board = Board.Board(boardSize)
@@ -23,13 +24,16 @@ class GameManager:
 
 
 if __name__ == "__main__":
-    GM = GameManager()
+    boardSize = 8
+    GM = GameManager(boardSize)
     GM.board.setAllPionPosition(GM.currentPlayer, GM.oppositePlayer)
     GM.printInfo()
     print("=====================================================")
-    GM.currentPlayer.movePion(14, Posisi.Posisi(4, 1), GM.board)
+    GM.currentPlayer.movePion(9, Posisi.Posisi(3, 1), GM.board)
     GM.printInfo()
     print("=====================================================")
-    GM.currentPlayer.movePion(14, Posisi.Posisi(4, 2), GM.board)
+    GM.currentPlayer.movePion(9, Posisi.Posisi(3, 2), GM.board)
     GM.printInfo()
     print("=====================================================")
+    print(Minimax.Z_Function(GM.currentPlayer, boardSize))
+    print(Minimax.Z_Function(GM.oppositePlayer, boardSize))

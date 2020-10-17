2e4e144c3edf7cde0f78175549c6523cc74aad2d
michaelhans <michaelhans777@gmail.com>
2020-10-17 13:21:44 +0700

    GameManager completed

--- src/GameManager.py
+++ src/GameManager.py
@@ -6,7 +6,7 @@ import Cell
 import GameState
 # import Minimax
 
-boardSize = 4
+boardSize = 10
 
 
 class GameManager:
@@ -23,6 +23,16 @@ class GameManager:
         temp = self.currentPlayer
         self.currentPlayer = self.oppositePlayer
         self.oppositePlayer = temp
+
+    def loadMenu(self):
+        print(
+            "Do you want to load the existing game? Tekan ENTER bila ingin new game!",
+            end=" ")
+        response = input()
+        if (len(response) > 0):
+            textFile = input("Masukkan nama file yang ingin diload: ")
+            self.loadGame(textFile)
+            print("LOAD GAME SUCCESSFULLY!", end="\n\n")
 
     def saveGame(self):
         textFile = input("Masukkan nama file sebagai output save game: ")
@@ -67,16 +77,18 @@ class GameManager:
         self.board.setAllPionPosition(self.currentPlayer, self.oppositePlayer)
         f.close()
 
-    def playing(self):
-        print(
-            "Do you want to load the existing game? Tekan ENTER bila ingin new game!",
-            end=" ")
-        response = input()
-        if (len(response) > 0):
-            textFile = input("Masukkan nama file yang ingin diload: ")
-            self.loadGame(textFile)
-            print("LOAD GAME SUCCESSFULLY!", end="\n\n")
+    def startGame(self, choice):
+        self.loadMenu()
+        if (choice == 1):
+            self.playing()
+        elif (choice == 2):
+            self.playerVsMinMax()
+        elif (choice == 3):
+            self.playerVsMinMaxLocalSearch()
+        elif (choice == 4):
+            self.botVsBot()
 
+    def playing(self):
         terminalState = False
         while not (terminalState):
             # Masukkan mau pion ID berapa yang mau dijalankan
@@ -105,7 +117,124 @@ class GameManager:
         self.printInfo()
         print("Player " + str(self.currentPlayer.noPlayer) + " win the game!")
 
+    def botVsBot(self):
+        terminalState = False
+        while not (terminalState):
+            # Masukkan mau pion ID berapa yang mau dijalankan
+            self.printInfo()
+            if (self.currentPlayer.noPlayer == 1):
+                print("PLAYER " + str(self.currentPlayer.noPlayer) +
+                      " MINIMAX TURN!")
+                self.currentPlayer.printAllPion()
+                # Minimax Process
+                # minMaxID, minMaxPosition = minMaxProcess()
+                # self.currentPlayer.movePion(minMaxID, minMaxPosition, self.board)
+            else:
+                print("PLAYER " + str(self.currentPlayer.noPlayer) + " TURN!")
+                self.currentPlayer.printAllPion()
+                # Minimax with Local Search
+                # minMaxID, minMaxPosition = minMaxProcess()
+                # self.currentPlayer.movePion(minMaxID, minMaxPosition, self.board)
+
+            if (self.board.checkTerminalState(self.currentPlayer.noPlayer)):
+                terminalState = True
+            else:
+                self.nextTurn()
+        self.printInfo()
+        print("Player " + str(self.currentPlayer.noPlayer) + " win the game!")
+
+    def playerVsMinMaxLocalSearch(self):
+        terminalState = False
+        while not (terminalState):
+            # Masukkan mau pion ID berapa yang mau dijalankan
+            self.printInfo()
+            if (self.currentPlayer.noPlayer == 1):
+                print("PLAYER " + str(self.currentPlayer.noPlayer) +
+                      " MINIMAX TURN!")
+                self.currentPlayer.printAllPion()
+                # Minimax Local Search
+                # minMaxID, minMaxPosition = minMaxProcess()
+                # self.currentPlayer.movePion(minMaxID, minMaxPosition, self.board)
+            else:
+                print("PLAYER " + str(self.currentPlayer.noPlayer) + " TURN!")
+                self.currentPlayer.printAllPion()
+                chosenID = int(
+                    input("Pilih Pion ID mana yang ingin dimainkan :  "))
+                if (chosenID == 999):
+                    self.saveGame()
+                possible_moves = self.currentPlayer.listAllPossibleMove(
+                    chosenID, self.board)
+                for i in range(len(possible_moves)):
+                    print(str(i + 1) + ". ", end="")
+                    possible_moves[i].printPosisi()
+                chosenMove = int(
+                    input(
+                        "Pilih move yang diinginkan dengan memasukkan nomor :  "
+                    ))
+                self.currentPlayer.movePion(chosenID,
+                                            possible_moves[chosenMove - 1],
+                                            self.board)
+
+            if (self.board.checkTerminalState(self.currentPlayer.noPlayer)):
+                terminalState = True
+            else:
+                self.nextTurn()
+        self.printInfo()
+        print("Player " + str(self.currentPlayer.noPlayer) + " win the game!")
+
+    def playerVsMinMax(self):
+        terminalState = False
+        while not (terminalState):
+            # Masukkan mau pion ID berapa yang mau dijalankan
+            self.printInfo()
+            if (self.currentPlayer.noPlayer == 1):
+                print("PLAYER " + str(self.currentPlayer.noPlayer) +
+                      " MINIMAX TURN!")
+                self.currentPlayer.printAllPion()
+                # Minimax Process
+                # minMaxID, minMaxPosition = minMaxProcess()
+                # self.currentPlayer.movePion(minMaxID, minMaxPosition, self.board)
+            else:
+                print("PLAYER " + str(self.currentPlayer.noPlayer) + " TURN!")
+                self.currentPlayer.printAllPion()
+                chosenID = int(
+                    input("Pilih Pion ID mana yang ingin dimainkan :  "))
+                if (chosenID == 999):
+                    self.saveGame()
+                possible_moves = self.currentPlayer.listAllPossibleMove(
+                    chosenID, self.board)
+                for i in range(len(possible_moves)):
+                    print(str(i + 1) + ". ", end="")
+                    possible_moves[i].printPosisi()
+                chosenMove = int(
+                    input(
+                        "Pilih move yang diinginkan dengan memasukkan nomor :  "
+                    ))
+                self.currentPlayer.movePion(chosenID,
+                                            possible_moves[chosenMove - 1],
+                                            self.board)
+
+            if (self.board.checkTerminalState(self.currentPlayer.noPlayer)):
+                terminalState = True
+            else:
+                self.nextTurn()
+        self.printInfo()
+        print("Player " + str(self.currentPlayer.noPlayer) + " win the game!")
+
 
 if __name__ == "__main__":
+    print("Welcome to the Halma Game!")
+    print("1. Multiplayer")
+    print("2. Player vs CPU Minimax")
+    print("3. Player vs CPU Minimax Local Search")
+    print("4. CPU Minimax vs CPU Minimax Local Search")
+    choice = int(input("Masukkan pilihan mode permainan yang diinginkan: "))
+    boardSize = int(input("Pilih ukuran papan N x N dalam input bilangan N: "))
+    tlimit = int(input("Tentukan time limit untuk setiap permainan! "))
+    playerChoice = 0
+    if (choice != 1) or (choice != 4):
+        playerchoice = int(
+            input("Tentukan mau pilih Merah (1) atau Hijau (2): "))
+
     GM = GameManager(boardSize)
-    GM.playing()
+    GM.startGame(choice)
